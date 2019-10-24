7f5ee486f232bf0ab5c709944c03009ce5933710
s458202 <moryah.dick@ocsbstudent.ca>
2019-10-24 13:35:48 -0400

    added to animating images

--- AnimatingImagesMoryah/main.lua
+++ AnimatingImagesMoryah/main.lua
@@ -1,7 +1,18 @@
 -----------------------------------------------------------------------------------------
---
+-- Title: AnimatingImages
+-- Name: Moryah
+-- Course: ICS2O/3C
+-- This program 
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background
+local background = display.newImageRect("Images/background.gif", 1100, 850)
+
+background.x = 500
+background.y = 400
+
