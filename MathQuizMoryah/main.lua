4091e271172a5f21eafc5a64a36007417f546673
Moryah D <moryah.dick@ocsbstudent.ca>
2019-11-01 15:18:59 -0400

    added to math quiz

--- MathQuizMoryah/main.lua
+++ MathQuizMoryah/main.lua
@@ -31,12 +31,17 @@ local num1
 local num2
 local numericField
 local numIncorrect = 0
-local points = 5
+local points = 0
 local pointsText
 local quesObject
 local randomOp
 local userAns
 local winObject
+
+-----------------------------------------------------------------------------------------
+
+local clockText
+local countDownTimer
 
 -----------------------------------------------------------------------------------------
 
@@ -53,7 +58,14 @@ local lostSound = audio.loadSound("Sounds/lose.mp3")
 local lostSoundChannel
 
 -----------------------------------------------------------------------------------------
--- creating first ball
+-- GLOBAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+TOTALSECONDS = 10
+secondsLeft = 10
+
+-----------------------------------------------------------------------------------------
+-- creating hearts
 local heart = display.newImage("Images/heart.png", 950, 66)
 	
 	heart.width = 100
@@ -76,8 +88,8 @@ local heart3 = display.newImage("Images/heart.png", 710, 66)
 local function AskQuestion()
 
 	-- chose a random operators
-	randomOp = math.random(1,4)
-
+	randomOp = math.random(1,5)
+	
 	-- chose a random operators
 	if(randomOp == 1) then
 
@@ -136,15 +148,87 @@ local function AskQuestion()
 
 		-- create question in text object
 		quesObject.text = num1 .. " / " .. num2 .. " = "
+
+	elseif(randomOp == 5) then
+
+		-- generate 2 numbers from 1 to 100
+		num1 = math.random(1, 20)
+		num2 = math.random(1, 5)
+			
+		-- the correct ans
+		correctAns = num1 ^ num2
+
+		-- create question in text object
+		quesObject.text = num1 .. " ^ " .. num2 .. " = "
 	end
+
+	local function roundToNthDecimal(correctAns, n)
+		local mult = 10^(n or 0)
+		return math.floor(correctAns * mult + 0.5) / mult
+	end
+end
+
+
+-- function that calls the timer
+function UpdateTime()
+	-- decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+	--timer.performWithDelay(1000)
+
+	-- display the number of seconds seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0 ) then
+
+		-- everytime user gets answer wrong take away 1 life
+		lives = lives - 1
+
+		-- taking away hearts
+		if (lives == 2) then
+
+			-- heart isn't visible
+			heart3.isVisible = false
+
+		elseif (lives == 1) then 
+
+			-- hearts aren't visible
+			heart3.isVisible = false
+			heart2.isVisible = false
+					
+		elseif (lives == 0) then 
+
+			-- hearts aren't visible
+			heart3.isVisible = false
+			heart2.isVisible = false
+			heart.isVisible = false
+
+			timer.cancel(countDownTimer)
+		end
+
+		-- reset the number of seconds left
+		secondsLeft = TOTALSECONDS
+	end
+end
+
+-- function that calls the timer
+function StartTimer(event)
+
+	-- create a countdown timer that loops infinitely
+	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+	countDownTimer.isVisible = true
+
+	-- coordinates
+	countDownTimer.x = 20
+	countDownTimer.y = 20
 end
 
 local function Hide()
 
-	lives.isVisible = true
+	correctObject.isVisible = false
+	countDownTimer.isVisible = true
+	incorrectObject.isVisible = false
 	loseObject.isVisible = false
-	points.isVisible = true
-	pointsText = false
+	pointsText.isVisible = true
 	winObject.isVisible = false
 	quesObject.isVisible = true
 	AskQuestion()
@@ -162,7 +246,7 @@ local function NumericFieldListener( event )
 		userAns = tonumber(event.target.text)
 
 		-- if the user ans is correct
-		if(user == correctAns) then
+		if(userAns == correctAns) then
 
 			correctSoundChannel = audio.play(correctSound)
 
@@ -176,7 +260,7 @@ local function NumericFieldListener( event )
 			pointsText.text = "points = " .. points
 
 			-- add timer
-			timer.performWithDelay(2000, Hide)
+			timer.performWithDelay(1000, Hide)
 
 			-- show "you win!" after 5 points		
 			if (points == 5) then
@@ -190,21 +274,59 @@ local function NumericFieldListener( event )
 				winObject.isVisible = true
 
 				-- add timer
-				timer.performWithDelay(2000, Hide)
+				timer.performWithDelay(3000, Hide)
 			end
 
 		else -- if user ans is wrong
 
 			incorrectSoundChannel = audio.play(incorrectSound)
 
+			incorrectObject.isVisible = true
+
 			-- create incorrect text object
-			incorrectObject.text = ("Inncorect! The correct answer was " .. correctAns .. " .DK ")
+			incorrectObject.text = "Inncorect! The correct answer was " .. correctAns .. " .DK "
 
 			-- everytime user gets answer wrong add 1 to numIncorrect
 			numIncorrect = numIncorrect + 1
 
+			-- everytime user gets answer wrong take away 1 life
+			lives = lives - 1
+
 			-- add timer
-			timer.performWithDelay(2000, Hide)
+			timer.performWithDelay(1000, Hide)
+
+			-- taking away hearts
+			if (lives == 2) then
+
+				-- heart isn't visible
+				heart3.isVisible = false
+
+			elseif (lives == 1) then 
+
+				-- hearts aren't visible
+				heart3.isVisible = false
+				heart2.isVisible = false
+
+			elseif (lives == 0) then 
+
+				lostSoundChannel = audio.play(lostSound)
+
+				-- this isn't visible
+				incorrectObject.isVisible = false
+
+				-- this is visible
+				loseObject.isVisible = true
+				
+				-- added timer
+				timer.performWithDelay(3000, Hide)	
+
+				-- hearts aren't visible
+				heart3.isVisible = false
+				heart2.isVisible = false
+				heart.isVisible = false
+
+				timer.cancel(countDownTimer)
+			end
 
 			-- display game over after 3 wrong answers
 			if (numIncorrect == 3) then
@@ -218,10 +340,14 @@ local function NumericFieldListener( event )
 				loseObject.isVisible = true
 				
 				-- added timer
-				timer.performWithDelay(2000, Hide)
+				timer.performWithDelay(3000, Hide)
+
+				-- hearts aren't visible
+				heart3.isVisible = false
+				heart2.isVisible = false
+				heart.isVisible = false
 			end
 		end
-
 		-- clear text field
 		event.target.text = ""		
 	end
@@ -232,14 +358,14 @@ end
 -----------------------------------------------------------------------------------------
 
 -- displays a question and sets the colour
-quesObject = display.newText( "", display.contentWidth/3, display.contentHeight/2,
+quesObject = display.newText( "", display.contentWidth/3.1, display.contentHeight/2,
 	 nil, 50 )
 	quesObject:setTextColor(1, 1, 1)
 
 -- create the correct text object object and make it invisible
 correctObject = display.newText( "Correct!", display.contentWidth/2, 
 	display.contentHeight
-	*2/3, nil, 50 )
+	*2/3, nil, 40 )
 	correctObject:setTextColor(155/255, 42/255, 198/255)
 	correctObject.isVisible = false
 
@@ -247,7 +373,7 @@ correctObject = display.newText( "Correct!", display.contentWidth/2,
 -- create the incorrect text object object and make it invisible
 incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, 
 	display.contentHeight
-	*2/3, nil, 50 )
+	*2/3, nil, 40 )
 	incorrectObject:setTextColor(155/255, 42/255, 198/255)
 	incorrectObject.isVisible = false
 
@@ -277,9 +403,13 @@ numericField:addEventListener( "userInput", NumericFieldListener)
 -- display the amount of points as a text object
 pointsText = display.newText("point = " .. points, display.contentWidth/3, display.
 	contentHeight/3, nil, 50)
+
+clockText = display.newText("" .. secondsLeft, 55, 55, nil, 110)
+
 -----------------------------------------------------------------------------------------
 -- FUNTION CALLS
 -----------------------------------------------------------------------------------------
 
 --call the function to ask the question
 AskQuestion()
+StartTimer()
